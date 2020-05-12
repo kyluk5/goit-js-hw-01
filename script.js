2e030c370b6f8df065147eea0082558f7db667c6
Andrii Kulyk <kyluk5@gmail.com>
2020-05-13 00:37:57 +0300

    first_done

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-import "./tasks/task_1";
-// import "./tasks/task_2";
-// import "./tasks/task_3";
-// import "./tasks/task_4";
-// import "./tasks/task_5";
-// import "./tasks/task_6";
+import "./tasks/task_1.js";
+// import "./tasks/task_2.js";
+// import "./tasks/task_3.js";
+// import "./tasks/task_4.js";
+// import "./tasks/task_5.js";
+// import "./tasks/task_6.js";
--- tasks/task_1.js
+++ tasks/task_1.js
@@ -1,1 +1,7 @@
 'use strict';
+
+const name = 'Генератор защитного поля';
+let prise = '1000';
+prise = '2000';
+console.log(`Выбран ${name}, цена за штуку ${prise} кредытов`);
+
