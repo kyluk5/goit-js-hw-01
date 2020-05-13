3116e6c8f5042a0e148106c9b5b40bd432a08286
Andrii Kulyk <kyluk5@gmail.com>
2020-05-13 14:41:15 +0300

    fourth_done

---
+++ tasks/task_4.js
@@ -0,0 +1,23 @@
+'use strict';
+
+let credits = 23580;
+const pricePerDroid = 3000;
+
+let input;
+input = Number(input);
+input = prompt('Сколько дроидов вы хотите купить?');
+
+let totalPrice = (input * pricePerDroid);
+
+let rest = (credits - totalPrice);
+rest = Number(rest);
+
+if (input === null) {
+    console.log('Отменено пользователем!');
+}
+else if (input * pricePerDroid > credits) {
+    console.log('Недостаточно средств на счету!');
+}
+else if (totalPrice <= credits) {
+    input = prompt(`Вы купили ${input} дроидов, на счету осталось ${rest} кредитов.`);
+}
