eddfa5ad92abe5dc60f25dbbdc00dc0ee99567b9
Andrii Kulyk <kyluk5@gmail.com>
2020-05-13 12:34:08 +0300

    third_done

--- tasks/task_3.js
+++ tasks/task_3.js
@@ -5,3 +5,13 @@ let message;
 
 message = prompt('Введите пароль');
 
+if (message === ADMIN_PASSWORD)
+    message = prompt('Добро пожаловать!');
+
+else if (message != ADMIN_PASSWORD)
+    message = prompt('Доступ запрещен, неверный пароль!')
+
+else (message === null)
+message = ('Отменено пользователем!');
+
+alert(message);
